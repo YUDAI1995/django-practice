b08dca23706ba35f59642c2b4ded983756d6e3d2
yudaiy <y.yamamoto991103@gmail.com>
2022-01-07 13:06:41 +0900

    config for deploy

--- django_practice/settings.py
+++ django_practice/settings.py
@@ -125,3 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Heroku deploy config
+import django_heroku
+django_heroku.settings(locals())
